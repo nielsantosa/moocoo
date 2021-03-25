d91de1b1028e16ab867c058983f27e714f016b8c
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-03-25 23:34:46 +0800

    Tidy up articles

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -2,13 +2,12 @@ class AnnotationsController < ApplicationController
   def create
     video = Video.find(annotation_params[:video])
     product = Product.find(annotation_params[:product])
-    time_start = annotation_params[:time_start]
-    time_end = annotation_params[:time_end]
     @annotation = Annotation.new(
       video: video,
       product: product,
-      time_start: time_start,
-      time_end: time_end)
+      time_start: annotation_params[:time_start],
+      time_end: annotation_params[:time_end]
+    )
     @annotation.save
     redirect_to video_path(video)
   end
